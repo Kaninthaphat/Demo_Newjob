d4e8383bb5552c8ae03b9121ae2118ad92d47d38
boyru <boyru@DESKTOP-436L88D>
2020-08-11 00:21:16 +0700

    update check

--- src/view/view_check.java
+++ src/view/view_check.java
@@ -8,16 +8,34 @@ public class view_check {
 		
 		String s;
 		Scanner scan = new Scanner(System.in);
-	    System.out.println("Enter a character:");
+	    System.out.println("Enter input:");
 	    s = scan.nextLine();
-		System.out.println(stringWithPosition(s));		
+		System.out.println(stringWithPosition(s));
     }
 	
 	public static String stringWithPosition(String s){
-		String pos = "";
-		for(int i = 0; i < s.length(); i++){
-			pos += (i);
+		
+		String a = "All Capital Letter";
+		String b = "All Small Letter";
+		String c = "Mix";
+		String d = "Invalid Input";
+		String ans;
+		String num;
+		for(int i =0 ; i<s.length(); i++){
+			char aa = s.charAt(i);
+            if(Character.isDigit(aa)){
+            	ans = d;
+            }
 		}
-		return String.format("%s\n%s", pos,s);
+		String upperStr = s.toUpperCase();
+		String lowerStr = s.toLowerCase();
+		if(s.equals(upperStr)){
+			ans = a;
+		}else if(s.equals(lowerStr)){
+			ans = b;
+		}else{
+			ans = c;
+		}
+		return ans;
 	}
 }
