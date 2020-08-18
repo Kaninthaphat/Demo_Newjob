14013e69ea1a3199f5e11468d4ca93d067fd739d
boyru <boyru@DESKTOP-436L88D>
2020-08-18 22:13:57 +0700

    update substring 3 4

--- src/view/view_substring_4.java
+++ src/view/view_substring_4.java
@@ -5,15 +5,18 @@ import java.util.Arrays;
 public class view_substring_4 {
 
 	public static void main(String[] args) {
+		
 		String s = "abba";
 		String ans[] = getCharArray(s);
 		System.out.print(Arrays.toString(ans));
 		
 	}
 	
-	public static String[] getCharArray(String str){
-		String ch[] = str.split("",2);
-		return ch;
+	public static String[] getCharArray(String s){
+		String strArr[] = new String[2];
+		strArr[0] = s.substring(0,2);
+		strArr[1] = s.substring(2,s.length());
+		return strArr;
 	}
 
 }
